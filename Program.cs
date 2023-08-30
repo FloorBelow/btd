18cc6b1998de81ac3b48fe0e69973f546a4cd7ee
FloorBelow <floorbelow@zoho.com>
2023-08-30 16:42:43 +1200

    full lod3 height export

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using LibDeflate;
 using ImageMagick;
 using System.Buffers;
+using System.Reflection;
 
 namespace BTD_Tests {
     internal class Program {
@@ -12,7 +13,9 @@ namespace BTD_Tests {
             //btd.WriteLod4Ltex();
             //btd.WriteLod4Vcol();
 
-            for(int i = 0; i < btd.lod3Offsets.Length / 2; i++) btd.WriteLod3Height(i);
+            btd.WriteLod3Full();
+
+            //for(int i = 0; i < btd.lod3Offsets.Length / 2; i++) btd.WriteLod3Height(i);
             Console.WriteLine("DONE");
         }
     }
@@ -23,8 +26,15 @@ namespace BTD_Tests {
         public string path;
         public long zlibOffset;
 
+        //helpful helper variaables
         public int cellsX;
         public int cellsY;
+        public int lod3CellsX;
+        public int lod3CellsY;
+        public int lod2CellsX;
+        public int lod2CellsY;
+        public int lod1CellsX;
+        public int lod1CellsY;
 
 
         public int version;
@@ -76,6 +86,12 @@ namespace BTD_Tests {
 
                 cellsX = maxX - minX + 1; //does this only work if max and min are different sign?
                 cellsY = maxY - minY + 1;
+                lod3CellsX = (cellsX + 7) / 8;
+                lod3CellsY = (cellsY + 7) / 8;
+                lod2CellsX = (cellsX + 3) / 4;
+                lod2CellsY = (cellsY + 3) / 4;
+                lod1CellsX = (cellsX + 1) / 2;
+                lod1CellsY = (cellsY + 1) / 2;
 
                 ltex = new uint[reader.ReadInt32()];
                 for(int i =  0; i < ltex.Length; i++) { ltex[i] = reader.ReadUInt32(); }
@@ -105,19 +121,19 @@ namespace BTD_Tests {
                 lod4Vcol = new ushort[cellsX * 8 * cellsY * 8];
                 for (int i = 0; i < lod4Vcol.Length; i++) lod4Vcol[i] = reader.ReadUInt16();
 
-                lod3Offsets = new uint[(cellsX + 7) / 8 * (cellsY + 7) / 8 * 2]; lod3Sizes = new int[lod3Offsets.Length];
+                lod3Offsets = new uint[lod3CellsX * lod3CellsY * 2]; lod3Sizes = new int[lod3Offsets.Length];
                 for(int i = 0; i < lod3Offsets.Length; i++) {
                     lod3Offsets[i] = reader.ReadUInt32();
                     lod3Sizes[i] = reader.ReadInt32();
                 }
 
-                lod2Offsets = new uint[(cellsX + 3) / 4 * (cellsY + 3) / 4 * 2]; lod2Sizes = new int[lod2Offsets.Length];
+                lod2Offsets = new uint[lod2CellsX * lod2CellsY * 2]; lod2Sizes = new int[lod2Offsets.Length];
                 for (int i = 0; i < lod2Offsets.Length; i++) {
                     lod2Offsets[i] = reader.ReadUInt32();
                     lod2Sizes[i] = reader.ReadInt32();
                 }
 
-                lod1Offsets = new uint[(cellsX + 1) / 2 * (cellsY + 1) / 2]; lod1Sizes = new int[lod1Offsets.Length];
+                lod1Offsets = new uint[lod1CellsX * lod1CellsY]; lod1Sizes = new int[lod1Offsets.Length];
                 for (int i = 0; i < lod1Offsets.Length; i++) {
                     lod1Offsets[i] = reader.ReadUInt32();
                     lod1Sizes[i] = reader.ReadInt32();
@@ -138,6 +154,62 @@ namespace BTD_Tests {
 
                 zlibOffset = reader.BaseStream.Position;
             }
+        }
+
+        public void WriteLod3Full() {
+            int blockCellCount = 8;
+            int cellSize = 16;
+            int blockSize = blockCellCount * cellSize;
+
+            int fullWidth = cellsX * cellSize;
+            int fullHeight = cellsY * cellSize;
+            ushort[] fullHeights = new ushort[fullWidth * fullHeight];
+            for(int y = 0; y < fullHeight; y+=2) {
+                for(int x = 0; x < fullWidth; x+=2) {
+                    fullHeights[x + y * fullWidth] = lod4Heights[x / 2 + y / 2 * fullWidth / 2];
+                }
+            }
+
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(path))) {
+                using Decompressor decompressor = new ZlibDecompressor();
+                for(int cellY = 0; cellY < cellsY; cellY += blockCellCount) {
+                    for (int cellX = 0; cellX < cellsX; cellX += blockCellCount) {
+                        int zlibBlockIndex = (cellX / blockCellCount + cellY / blockCellCount * lod3CellsX) * 2;
+
+                        reader.BaseStream.Seek(zlibOffset + lod3Offsets[zlibBlockIndex], SeekOrigin.Begin);
+                        var status = decompressor.Decompress(reader.ReadBytes(lod3Sizes[zlibBlockIndex]), blockSize * blockSize * 3, out var decompressed);
+                        if (status != OperationStatus.Done) {
+                            Console.WriteLine(status);
+                            continue;
+                        }
+                        Span<ushort> blockShorts = MemoryMarshal.Cast<byte, ushort>(decompressed.Memory.Span);
+                        int blockWidth = cellX + blockCellCount > cellsX ? (cellsX % blockCellCount) * cellSize : blockSize;
+                        int blockHeight = cellY + blockCellCount > cellsY ? (cellsY % blockCellCount) * cellSize : blockSize;
+
+
+                        int i = 0;
+                        for (int y = 0; y < blockHeight; y += 2) {
+                            for (int x = 0; x < blockWidth; x += 2) {
+                                fullHeights[cellX * cellSize + cellY * fullWidth * cellSize + x + 1 + y * fullWidth] = blockShorts[x / 2 + y / 2 * 192];
+                            }
+                            for (int x = 0; x < blockWidth; x += 2) {
+                                fullHeights[cellX * cellSize + cellY * fullWidth * cellSize + x + y * fullWidth + fullWidth] = blockShorts[x + y / 2 * 192 + 64];
+                                fullHeights[cellX * cellSize + cellY * fullWidth * cellSize + x + 1 + y * fullWidth + fullWidth] = blockShorts[x + 1 + y / 2 * 192 + 64];
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            var span = MemoryMarshal.AsBytes(fullHeights.AsSpan());
+            MagickImage image = new MagickImage();
+            image.Read(span, new MagickReadSettings() { Width = fullWidth, Height = fullHeight, Depth = 16, Format = MagickFormat.Gray });
+            string filename = $"lod3heights.png";
+            Console.WriteLine(filename);
+            image.Write(filename);
+
+
         }
 
         public void WriteLod3Height(int index) {
